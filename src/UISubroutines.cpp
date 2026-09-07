26b5923cb520710768b6c4c098de19c0bbb323fb
AnClark Liu <clarklaw4701@qq.com>
2026-09-07 20:25:32 +0800

    ui: Use my own brand rather than original Kjaerhus logo
    To prevent possible infringements with the Kjaerhus brand.

--- src/UISubroutines.cpp
+++ src/UISubroutines.cpp
@@ -172,15 +172,15 @@ void ClassicReverbUI::_drawKjaerhusLogo(const ImVec2& size)
         draw_list->PathFillConcave(IM_COL32(255, 255, 255, 60));
     }
 
-    // "KJÆRHUS AUDIO" text
+    // "ANCLARK STUDIO" text
     ImGuiExt::AddTextScaled(draw_list, ImGui::GetIO().Fonts->Fonts[2], SCALE(20.0f),
                             ImVec2(pos.x + SCALE(10.0f), pos.y + SCALE(8.0f)),
                             IM_COL32(255, 255, 255, 255),
-                            "KJ\xc3\x86RHUS AUDIO", 0.65f, 1.0f);
+                            "ANCLARK STUDIO", 0.65f, 1.0f);
 
-    // "Recreated by AnClark" badge with semi-transparent background
+    // "Classic Series Reborn" badge with semi-transparent background
     {
-        const char* info_text      = "Recreated by AnClark";
+        const char* info_text      = "Classic Series Reborn";
         const float kFontSz    = SCALE(16.0f);
         const float kScaleX    = 0.8f;
         const float kScaleY    = 0.8f;
